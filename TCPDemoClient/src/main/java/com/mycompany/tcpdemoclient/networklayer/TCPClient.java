a1507cafaddd794e51c76f268a2a1a55c352d5f0
Samuel Sanchez <125204393+SamuelSV26@users.noreply.github.com>
2025-01-24 11:58:54 -0500

    Refactor TCPDemoClient and TCPClient classes to remove unnecessary comments and improve resource management

--- TCPDemoClient/src/main/java/com/mycompany/tcpdemoclient/networklayer/TCPClient.java
+++ TCPDemoClient/src/main/java/com/mycompany/tcpdemoclient/networklayer/TCPClient.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.tcpdemoclient.networklayer;
 
 import java.io.DataInputStream;
@@ -9,10 +5,6 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 
-/**
- *
- * @author santi
- */
 public class TCPClient {
     private String serverAddress;
     private int port;
