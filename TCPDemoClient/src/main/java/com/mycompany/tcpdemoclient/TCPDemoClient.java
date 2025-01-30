3140ffba07f1198e6c7afc844d80eff0c438724b
Samuel Sanchez <125204393+SamuelSV26@users.noreply.github.com>
2025-01-30 16:45:59 -0500

    Integrar cliente TCP en TCPDemoClient para enviar y recibir mensajes

--- TCPDemoClient/src/main/java/com/mycompany/tcpdemoclient/TCPDemoClient.java
+++ TCPDemoClient/src/main/java/com/mycompany/tcpdemoclient/TCPDemoClient.java
@@ -1,5 +1,7 @@
 package com.mycompany.tcpdemoclient;
 import java.util.Scanner;
+
+import com.mycompany.tcpdemoclient.networklayer.TCPClient;
 public class TCPDemoClient {
 
     public static void main(String[] args) {
@@ -8,7 +10,9 @@ public class TCPDemoClient {
             String name = in.nextLine();
             System.out.println("Type your last name");
             String lastName = in.nextLine();
-            System.out.println("Name: "+ name +  " Last Name: " + lastName);
+            TCPClient client =  new TCPClient("172.30.123.16", 9090);
+            String response = client.sendMesagge(name, lastName);
+            System.out.println(response);
         }
         
     }
